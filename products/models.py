6b18043f5a566a9d5abf827a4bc038fb21fc6ddd
TChauruka <takudzwachauruka@gmail.com>
2021-06-02 18:57:12 +0000

    customized products admin

--- products/models.py
+++ products/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 class Category(models.Model):
+    
+    # to fix the spelling mistake on the admin website
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null= True, blank=True)
 
