7814b5d22c01c7801c3fd8d3d61a9e4e1080fb14
TChauruka <takudzwachauruka@gmail.com>
2021-06-16 13:58:44 +0000

    removed secret key

--- space_patch/settings.py
+++ space_patch/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#3=ww8*p$@=-i-%@-ida@pvcwgkc9dp1xue1!89c9lx=u*&2xj'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['space-patch.herokuapp.com','localhost']
 
