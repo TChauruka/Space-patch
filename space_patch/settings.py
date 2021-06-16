7da19d92311e008ac04ffd462345e41cf36118fb
TChauruka <takudzwachauruka@gmail.com>
2021-06-16 15:42:21 +0000

    added cache control

--- space_patch/settings.py
+++ space_patch/settings.py
@@ -29,6 +29,12 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['space-patch.herokuapp.com','localhost']
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'space-patch'
     AWS_S3_REGION_NAME = 'eu-west-2'
