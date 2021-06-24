0a7d883c037b935a0dd4fa9f19a283bdfff8a9ed
TChauruka <takudzwachauruka@gmail.com>
2021-06-24 12:39:05 +0000

    changed the default email

--- space_patch/settings.py
+++ space_patch/settings.py
@@ -218,7 +218,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'spacepatch@example.com'
+    DEFAULT_FROM_EMAIL = 'spacepatch@spacepatch.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
