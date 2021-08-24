f470a7c731c6fe552a8701e94f3c2d3eb785f850
TChauruka <takudzwachauruka@gmail.com>
2021-08-24 09:04:44 +0000

    added blog models to database

--- space_patch/settings.py
+++ space_patch/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','')
+SECRET_KEY = os.environ.get('SECRET_KEY','7d51hdm5fzj$ldknra@#eyb#@_w(4#8$@@nzl9iqtg0je%!*ad')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'blog',
 
     # Other apps
     'crispy_forms', 
