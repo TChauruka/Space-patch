178d1700c3cf664d627028debdcb60808edfe4b3
TChauruka <takudzwachauruka@gmail.com>
2021-08-27 15:12:38 +0000

    added form for comments

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments',
                              on_delete=models.CASCADE)
     name = models.CharField( max_length=255)
-    email = models.EmailField
+    email = models.EmailField()
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CommentForm
 from .models import Post
 
 
@@ -11,4 +12,21 @@ def blog(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
 
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.method =='POST':
+        form = CommentForm(request.Post)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = CommentForm()
+
+
+
+
+
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'form':form })
