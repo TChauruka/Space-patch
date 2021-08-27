5c05a90e8b309356a3eccc25e246401b790674a7
TChauruka <takudzwachauruka@gmail.com>
2021-08-27 14:35:38 +0000

    added Commnet models to database

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-date_added']
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments',
+                             on_delete=models.CASCADE)
+    name = models.CharField( max_length=255)
+    email = models.EmailField
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added']
